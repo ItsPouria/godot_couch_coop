852277425e830b23612895c34078a102c2be96df
ItsPouria <pouriamemarvar@gmail.com>
2026-05-11 19:50:47 +0200

    add input maps to add_player function

--- multiplayer_setup.gd
+++ multiplayer_setup.gd
@@ -1,6 +1,8 @@
 extends Node
 
 var num_player = Input.get_connected_joypads().size()
+var players: Array = []
+var input_map: Array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,10 +13,22 @@ func gamepad_connection_changed(device: int, connected: bool):
 	if connected:
 		num_player = Input.get_connected_joypads().size()
 		print("Connected device {d}.".format({"d":device}))
-		add_player()
+		add_player(device)
 	else:
 		pass
 		#setup gamepad disconnected
 
-func add_player():
-	pass
+func add_player(device: int):
+	var player = load("res://player.tscn")
+	var player_instance = player.instantiate()
+	players.append(player_instance)
+	print(players)
+	
+	input_map.append({
+		"ui_up{n}".format({"n": device}): Vector2.UP,
+		"ui_right{n}".format({"n": device}): Vector2.RIGHT,
+		"ui_down{n}".format({"n": device}): Vector2.DOWN,
+		"ui_left{n}".format({"n": device}): Vector2.LEFT,
+		#TODO Add more input maps here
+	})
+	
